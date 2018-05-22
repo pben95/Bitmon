59851200ef062c8bbd7b43a1fbcae24bc89f01f3
Timothy Fisher <timothyjfisher@hotmail.com>
2018-05-22 15:57:54 +0100

    web3connect to test contract

--- bitmon/src/actions/web3Connect.js
+++ bitmon/src/actions/web3Connect.js
@@ -7,9 +7,8 @@ export const web3Connect = () => {
       let web3Location = `https://rinkeby.etherscan.io/address/0xf93b68309245da8dbc21fcac0756a71b13e23b47`
   
       let output = (typeof web3 !== 'undefined') // web3 given by metamask
-                    ? { type: types.WEB3_CONNECTED, payload: { web3: new Web3(web3.currentProvider), isConnected: true } }
-                    : { type: types.WEB3_DISCONNECTED, payload: { web3: null, isConnected: false } }  // comment out for optional section
-                     //: { type: WEB3_CONNECTED, payload: { web3: new Web3(new Web3.providers.HttpProvider(web3Location)), isConnected: true } }  // comment in for optional section
+                    ? { type: types.WEB3_CONNECTED, payload: { web3: new Web3(new Web3.providers.HttpProvider(web3Location)), isConnected: true } }  // comment in for optional section
+                     : { type: types.WEB3_DISCONNECTED, payload: { web3: null, isConnected: false } }  // comment out for optional section
       /*eslint-enable */
       dispatch(output)
     }
