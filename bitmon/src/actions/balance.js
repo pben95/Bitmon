c8e52baa3bdd6bbbd4e475ec7e0455ec0e05ba63
pben95 <pben95@users.noreply.github.com>
2018-05-24 21:22:14 -0400

    Update balance.js
    change contract address

--- bitmon/src/actions/balance.js
+++ bitmon/src/actions/balance.js
@@ -1,11 +1,12 @@
 import * as types from './types'
 import bitmonContract from '../contract/BitmonContract'
 import abi from '../contract/abi'
+var contractAddress = '0xF93b68309245DA8Dbc21fcac0756a71b13E23B47'
 
 export const getBalance = (account) => {
     return (dispatch, getState) => {
       return new Promise((resolve, reject) => {
-        let bitmon = bitmonContract(abi, account)
+        let bitmon = bitmonContract(abi, contractAddress)
         bitmon.methods.balanceOf(account).call()
           .then((value) => {
             dispatch({
