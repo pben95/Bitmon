8aaa2323e77da16a17697b7bfe81cb879a6d27d4
pben95 <pben95@users.noreply.github.com>
2018-05-24 21:19:34 -0400

    Update BitmonContract.js
    change node provider

--- bitmon/src/contract/BitmonContract.js
+++ bitmon/src/contract/BitmonContract.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 
 const BitmonContract = (ABI, account) => {
-    let web3Provider = new Web3(new Web3.providers.HttpProvider('https://rinkeby.etherscan.io/address/0xf93b68309245da8dbc21fcac0756a71b13e23b47'));
+    let web3Provider = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/R2rm8T1awO3hgUnGw5l2'));
 
     return new web3Provider.eth.Contract(ABI, account);
 }
