55b1feba398c95feb82911a47d4d9c46a327288b
pben95 <pben95@users.noreply.github.com>
2018-05-24 21:19:48 -0400

    Update web3Connect.js
    change node provider

--- bitmon/src/actions/web3Connect.js
+++ bitmon/src/actions/web3Connect.js
@@ -4,7 +4,7 @@ import * as types from './types'
 export const web3Connect = () => {
     return (dispatch, getState) => {
       /*eslint-disable */
-      let web3Location = `https://rinkeby.etherscan.io/address/0xf93b68309245da8dbc21fcac0756a71b13e23b47`
+      let web3Location = `https://rinkeby.infura.io/R2rm8T1awO3hgUnGw5l2`
   
       let output = (typeof web3 !== 'undefined') // web3 given by metamask
                     ? { type: types.WEB3_CONNECTED, payload: { web3: new Web3(new Web3.providers.HttpProvider(web3Location)), isConnected: true } }  // comment in for optional section
